6ce133ec1910eb13a15ffe2d6ac79e26c89a6a20
shub12388y <spl.sp999@gmail.com>
2025-04-10 00:53:25 +0530

    init

---
+++ apps/web/app/api/v1/posts/post/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async(request: NextRequest) => {
+  try {
+    const data = await request.json();
+    if (!data) {
+      return NextResponse.json(
+        { message: "Empty Request Body" },
+        { status: 404 }
+      );
+    }
+    
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Server Error" }, { status: 200 });
+  }
+};
